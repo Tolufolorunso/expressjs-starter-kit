7a417e93279488e67ad6c5b24939173ae7bddc4a
KolaTolulope <81151811+KolaTolulope@users.noreply.github.com>
2021-06-12 17:30:43 +0100

    updated

--- app.js
+++ app.js
@@ -1,48 +1,74 @@
-const express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    path = require('path'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser');
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+const PORT = process.env.PORT || 8000
+// const   favicon = require('serve-favicon')
 
-// Load DB connection
-const db = require('./models/db');
+dotenv.config({
+  path: './config.env',
+})
+const app = express()
 
-// Load user DB
-const users = require('./models/users');
+app.use(logger('dev'))
+app.use(express.json())
+app.use(cors())
 
-// Load templating and statics
-app.use(express.static(path.join(__dirname, 'public')));
+app.options('*', cors())
 
-const nunjucks = require('nunjucks');
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
-});
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
-app.set('view engine', 'html');
+// const nunjucks = require('nunjucks')
+// nunjucks.configure('views', {
+//   autoescape: true,
+//   express: app,
+// })
 
+// Route
+const imageRoute = require('./routes/imageRoutes')
+app.use('/api/v1/image', imageRoute)
 
-// Call routes
-app.use(require('./routes'))
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Api for My Unsplash Challenge',
+  })
+})
 
 // catch 404 and forward to error handler
 app.use((req, _res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    err.message = 'The page ' + req.hostname + req.originalUrl + ' could not be found on this website.';
-    next(err);
-});
+  const err = new Error('Not Found')
+  err.status = 404
+  err.message =
+    'The page ' +
+    req.hostname +
+    req.originalUrl +
+    ' could not be found on this website.'
+  next(err)
+})
 
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
+// MongoDB connection
+// const DB = process.env.DATABASE_LOCAL
+const DB = process.env.DATABASE
+
+// process.env.DATABASE_LOCAL
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((c) => console.log('DATABASE connection successfull'))
+
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT)
 })
 
 /*** Error handlers ***/
@@ -50,25 +76,26 @@ app.listen(port, () => {
 // Development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use((err, _req, res, _next) => {
-        res.status(err.status || 500);
-        res.render('error', {
-            title: 'Error ' + err.status,
-            message: err.message,
-            error: err
-        });
-    });
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500)
+    return res.json({
+      status: 'fail',
+      title: 'Error ' + err.status,
+      message: err.message,
+      error: err,
+    })
+  })
 }
 
 // Production error handler
 // no stacktraces leaked to user
 app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        title: 'Error ' + err.status,
-        message: err.message,
-        error: {}
-    });
-});
+  res.status(err.status || 500)
+  res.render('error', {
+    title: 'Error ' + err.status,
+    message: err.message,
+    error: {},
+  })
+})
 
-module.exports = app;
+module.exports = app
--- models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.set('useCreateIndex', true); // This pervent collection.ensureIndex depreciation warning
-mongoose.set('useFindAndModify', false); // This prevent current mongoose depreciation warning
-mongoose.connect('mongodb://localhost/starterkit', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
